2f350b359ecdfc3d81694d060111bf296af2ee51
CFranco-2012305 <cfranco-2012305@kinal.edu.gt>
2016-06-10 03:39:49 -0600

    app turrismo

---
+++ app/src/main/java/com/carlosfranco/vijuk/turismo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.carlosfranco.vijuk.turismo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    JsonObjectRequest array;
+    RequestQueue mRequestQueue;
+    private final String url = "http://192.168.1.22:8080/Proyecto/public/jugador";
+    private final String TAG = "Hope";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
