b450dcbe03f12c766e81f539a8eea6de74f79d54
CFranco-2012305 <cfranco-2012305@kinal.edu.gt>
2016-06-12 15:07:08 -0600

    no message

--- app/src/main/java/com/carlosfranco/vijuk/turismo/MainActivity.java
+++ app/src/main/java/com/carlosfranco/vijuk/turismo/MainActivity.java
@@ -3,6 +3,9 @@ package com.carlosfranco.vijuk.turismo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 public class MainActivity extends AppCompatActivity {
 
     JsonObjectRequest array;
